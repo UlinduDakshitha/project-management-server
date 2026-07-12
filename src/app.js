b9a8a87b17881a869c08585ca85c5427176fe0c9
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-07-12 23:23:23 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Project Management API Running"
+    });
+});
+
+module.exports = app;
